a19be00127c7d50f95bda64a6fd23e5c1dac2da4
prajayshah <prajay.shah@mail.utoronto.ca>
2023-11-19 15:44:44 -0500

    deleted conflicting math.py file

--- src/funcsforprajay/funcs.py
+++ src/funcsforprajay/funcs.py
@@ -7,6 +7,7 @@ from typing import Union
 import numpy as np
 import pandas as pd
 from scipy import stats, ndimage, io
+from scipy.optimize import curve_fit
 import itertools
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -486,6 +487,34 @@ def decay_timescale(arr, decay_constant=None, signal_rate=1):
 
     return timescale / signal_rate
 
+
+# logaritmic regression fit function
+def logarithmic_regression_fit(x, y):
+    """
+    Fit a logarithmic regression function to the data
+    :param x: x data
+    :param y: y data
+    :return: a, b
+    """
+    def log_func(x, a, b):
+        return a + b * np.log(x)
+
+    # Finding the optimal parameters :
+    popt, pcov = curve_fit(log_func, x, y)
+    print("a  = ", popt[0])
+    print("b  = ", popt[1])
+
+    # Predicting values:
+    y_pred = log_func(x, popt[0], popt[1])
+
+    # Check the accuracy :
+    from sklearn.metrics import r2_score
+    Accuracy = r2_score(y, y_pred)
+    print(f'R**2: {Accuracy}')
+
+    return popt[0], popt[1], y_pred
+
+############### HELPFUL FUNCTIONS FOR PLOTTING ##########################################################################
 
 # random func for rotating images and calculating the image intensity along one axis of the image
 def rotate_img_avg(input_img, angle):
